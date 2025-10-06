4cad8e6a51608c9ba8dc8cf51156f88debc4697c
valeriogt <vgtvaleriogarcia@gmail.com>
2025-10-06 09:27:14 +0200

    Update ejercicio7.js

---
+++ ejercicio7.js
@@ -0,0 +1,11 @@
+function greaterNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne);
+  } else {
+    console.log(numberTwo);
+  }
+}
+
+// Ejemplo de uso:
+greaterNumber(5, 8); // Muestra 8
+greaterNumber(12, 7); // Muestra 12
