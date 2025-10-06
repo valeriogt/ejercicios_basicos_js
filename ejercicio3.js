204f313515996a5322e6e63ddebf1424ffa4bb6f
valeriogt <vgtvaleriogarcia@gmail.com>
2025-10-06 09:26:21 +0200

    Update ejercicio3.js

---
+++ ejercicio3.js
@@ -0,0 +1,20 @@
+// 3.1 Multiplica 10 por 5 y muestra el resultado
+console.log(10 * 5); // Resultado: 50
+
+// 3.2 Divide 10 por 2 y muestra el resultado
+console.log(10 / 2); // Resultado: 5
+
+// 3.3 Muestra el resto de dividir 15 por 9
+console.log(15 % 9); // Resultado: 6
+
+// 3.4 Usa el operador de asignación para que o = 15, teniendo p = 10 y j = 5
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o); // Resultado: 15
+
+// 3.5 Usa el operador de asignación para que i = 50, teniendo c = 10 y m = 5
+let c = 10;
+let m = 5;
+let i = c * m;
+console.log(i); // Resultado: 50
